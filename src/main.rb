791e8505ffcfd4fff77ca260a6a6df97338c0218
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-04-24 20:23:53 +0800

    Refactor

--- src/main.rb
+++ src/main.rb
@@ -6,55 +6,44 @@ load 'spider.rb'
 WHICH_CHILD = 4
 
 def main
-	anime_id = promptAnime
-	anime_title = getTitleFromId anime_id
-	anime_title.tr! ' ', '_'
-	anime_chars_url = "https://myanimelist.net/anime/" + anime_id.to_s + "/" + anime_title + "/characters"
-	puts getSeiyuuList URI.encode(anime_chars_url)
-end
-
-# prompt user for anime id
-def promptAnime
 	puts "Enter anime name/id: "
 	anime_id = gets.chomp
 	puts "Please wait..."
-	anime_id = getIdFromRoughName anime_id unless looksValidId? anime_id
-	return anime_id
+	
+	if not /^[0-9]{1,}$/.match anime_id then
+		query = Jikan::Query.new
+		results = query.search anime_id, :anime
+		anime_id = results.id[0]
+	end
+	
+	seiyuu_list = getSeiyuuList anime_id
+	puts seiyuu_list
+
+	#completed_list = getCompletedList
 end
 
-def looksValidId? str
-	regex = /^[0-9]{1,}$/
-	return regex.match str
-end
+def getSeiyuuList id
 
-# anime title -> anime id
-def getIdFromRoughName name
-	query = Jikan::Query.new
-	results = query.search name, :anime
-	return results.id[0]
-end
-
-# anime id -> anime title
-def getTitleFromId id
-	anime = Jikan::anime id
-	return anime.title
-end
-
-def getSeiyuuList url
-
+	anime_title = (Jikan::anime id).title
+	anime_title.tr! ' ', '_'
+	url = URI.encode("https://myanimelist.net/anime/" + id.to_s + "/" + anime_title + "/characters")
+	
 	doc = Nokogiri::HTML open url
 
 	elems = doc.css "tr td a"
-	returnVal = []
+	return_val = []
 	
 	elems.each do |link, x|
-		returnVal.push link.parent.children[1].content.tr ",","" if link["href"] and link["href"].include? "/people/" and
+		return_val.push link.parent.children[1].content.tr ",","" if link["href"] and link["href"].include? "/people/" and
 			link.parent.children[WHICH_CHILD] and link.parent.children[WHICH_CHILD].content == "Japanese"
 	end
 
-	returnVal.uniq!
+	return_val.uniq!
+	return return_val
+end
 
-	return returnVal
+def getCompletedList
+	return ["love live", "mirai nikki", "tokyo ghoul", "madoka magica", "re", "kuroko no basket"]
 end
 
 main
